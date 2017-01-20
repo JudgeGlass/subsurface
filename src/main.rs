bd9d1d8f43ed7311ad401dbb3e1fb9e972725e9a
Nitori- <nat1192@gmail.com>
2017-01-19 23:07:12 -0600

    One window to paint them all

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate glium;
+extern crate glutin;
+
 fn main() {
-    println!("Hello, world!");
+    use glium::DisplayBuild;
+    use glium::Surface;
+
+    let display = glium::glutin::WindowBuilder::new()
+        .with_dimensions(1024, 768)
+        .with_title(format!("Hello world"))
+        .build_glium()
+        .unwrap();
+
+    loop {
+        for event in display.poll_events() {
+            match event {
+                glutin::Event::Closed => return,
+                _ => (),
+            }
+        }
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+    }
 }
